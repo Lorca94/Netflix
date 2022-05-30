1c3db9f0575fe1471473934f9adcbe7046f53f14
Lorca94 <lorcanavarrov@gmail.com>
2022-05-30 22:43:15 +0200

    introduccion a página principal

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
+import React from "react";
+import inicioData from './fixtures/jumbo.json';
+import Inicio from "./components/inicio"
+
 export default function App() {
   return (
-    <p>Hola</p>
+    <Inicio.Container>
+      {
+        inicioData.map((item) => (
+          <Inicio key={item.id} direction={item.direction}>
+            <Inicio.Title>{item.title}</Inicio.Title>
+            <Inicio.SubTitle>{item.subTitle}</Inicio.SubTitle>
+            <Inicio.Image src= {item.image} alt = {item.alt} />
+          </Inicio>
+        ))
+      }
+    </Inicio.Container>
   );
 }
