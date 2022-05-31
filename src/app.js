bcc111885b4765207ae56dcf71e145d1a4fbccec
Lorca94 <lorcanavarrov@gmail.com>
2022-05-31 23:18:35 +0200

    añadido routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import React from "react";
-import FooterContainer from "./containers/footer";
-import JumbotronContainer from "./containers/jumbotron";
-import FaqsContainer from "./containers/faqs";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Browse, Home, SingIn, SingUp } from "./pages";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route exact path={ROUTES.SIGN_IN} element={<SingIn />} />
+        <Route exact path={ROUTES.SIGN_UP} element={<SingUp />} />
+        <Route exact path={ROUTES.BROWSE} element={<Browse />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './home';
+export { default as Browse } from './browse';
+export { default as SingIn } from './signin';
+export { default as SingUp } from './signup';
